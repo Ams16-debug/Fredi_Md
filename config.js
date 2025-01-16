28339536999483500d27b57cdcce3c908e35e2a7
Ams16-debug <mofiyinfoluwaadeyoye@gmail.com>
2025-01-16 10:18:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "cHsgyBbZ#AuQsMd9egrbjITBjE4F-RWsHXLf4s_5kvWtYj5DGrBQ",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
@@ -17,7 +17,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255752593977",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "2348074553808",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "✧FREDIE✧",
 // add bot owner name
